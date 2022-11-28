8bf84ddd3aebe485f28154e5996c56854ca58f96
misha-solntcev <misha.solntcev@gmail.com>
2022-11-28 18:07:05 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 12. Дано целое число N (> 1). Вывести наибольшее из целых чисел K, 
+    для которых сумма 1 + 2 + … + K будет меньше или равна N, 
+    и саму эту сумму. */
+
+namespace Lab_3_1_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 104;
+            int sum = 0;
+            int K = 0;
+            while (sum <= N)
+            {
+                K++;                               
+                sum += K;
+                Console.WriteLine($"K = {K}, sum = {sum}");
+            }            
+            Console.ReadKey();
+        }
+    }
+}
